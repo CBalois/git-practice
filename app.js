11f0654fbd0ff6d6a39c60906bd22a380d7ed80b
Camelle <baloiscamelle@gmail.com>
2024-09-07 21:25:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 console.log("Hello, World!");
-let name = "camelle"
-console.log("My name is," + name);
+let myname ="Camelle Balois";
+console.log('My name is'+ myname);
+
+console.log('USING VARIABLES');
+let name = "Camelle Balois";
+let age = "20";
+console.log("Hello! I am " + name + ";" + age + "years old. ");
+
+console.log("USING NUMBERS");
+const a = 1 ;
+const b = 2 ;
+
+const sum = x + y;
+console.log(sum);
+
+console.log('USING BOOLEANS');
+let c = true;
+let d = false;
+console.log("I am learning programming",c);
+console.log("I will challenge myself",d);
+
+console.log('COMPARISON OPERATORS SECTION');
+let e = 3 ;
+let f = 4 ;
+
+let g = e > f;
+console.log("Is e more than f?"+ g);
+let h = e < f;
+console.log("Is e less than f?"+ h);
+let i = e === f;
+console.log("Is e equal to f?"+ i);
+let j = e != f;
+console.log ("Is not equal to f?"+ j);
+ 
+
+
+
+
+
+
+
 
