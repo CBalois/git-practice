43f06ab0bff9624d3b630635fc5dcd4a513b6134
Camelle <baloiscamelle@gmail.com>
2024-09-09 16:43:04 +0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "Camelle";
+let whatDoYouWannaBecomeInYpurLife = "programmer";
+let gender = "Female";
+let twitterHandle = "none";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYpurLife);
+console.log(gender);
+console.log(twitterHandle);
