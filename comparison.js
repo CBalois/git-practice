9f207c5c69e0790cc31db4202f2a2985af3c40db
Camelle <baloiscamelle@gmail.com>
2024-09-13 11:36:06 +0800

    Update comparison.js

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNum = 25;
+let secondFavNum= 22;
+
+//greater than
+console.log(firstFavNum > secondFavNum);
+
+//less than
+console.log(firstFavNum < secondFavNum); 
+
+//greater than or equal to
+console.log(firstFavNum >= secondFavNum); 
+
+//less than to equal to
+console.log(firstFavNum <= secondFavNum); 
+
+//strict equality (===)
+console.log(firstFavNum === secondFavNum); 
+
+// loose equality  (==)
+console.log(firstFavNum == secondFavNum);
+
+//strict inequality (!==)
+console.log(firstFavNum !== secondFavNum);
+
+//loose inequality (!=)
+console.log(firstFavNum != secondFavNum);
+
+//AND operator
+console.log(firstFavNum > 25 && secondFavNum < 22);
+
+//OR operator
+console.log(firstFavNum < 25 || secondFavNum > 22);
+
+//NOT operator
+console.log(!(firstFavNum > secondFavNum));
